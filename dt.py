414dc42733470ed9527732a0fcf188e40d98ec4e
kerminaFekry <73202783+kerminaFekry@users.noreply.github.com>
2021-10-07 06:20:46 +0200

    user_defined  functions in python

---
+++ dt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def greet(name):
+    
+    print("Hello, " + name + ". Good morning!")
+name=input("what is you name ? ")   
+out=greet(name)
+
+
+# In[8]:
+
+
+def add_numbers(x,y):
+   sum = x + y
+   return sum
+
+num1 = int(input("enter the first number : "))
+num2 = int(input("enter the second number : "))
+
+print("The sum is", add_numbers(num1, num2)) 
+
+
+# In[ ]:
+
+
+
+
